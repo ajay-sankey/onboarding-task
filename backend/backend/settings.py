5eee9231792f50d59353aaccf2ab9334ca597a72
ajay-sankey <ajay.d@sankeysolutions.com>
2024-02-27 17:38:01 +0530

    fix: added CORS headers (backend)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,15 +44,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'employees.apps.EmployeesConfig',
-    'airflow.apps.AirflowConfig'
+    'airflow.apps.AirflowConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
